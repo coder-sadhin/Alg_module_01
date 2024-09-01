5d0b51f42180b13b311c5fb83aa0de4627c472d4
coder-sadhin <codersadhin@gmail.com>
2024-09-01 21:35:13 +0600

    adjacency input

--- adjacency_list.cpp
+++ adjacency_list.cpp
@@ -5,7 +5,13 @@ int main()
     int n, e;
     cin >> n >> e;
     vector<int> mat[n];
-    
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        mat[a].push_back(b);
+        mat[b].push_back(a);
+    }
     
     return 0;
 }
