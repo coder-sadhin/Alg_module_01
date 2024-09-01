3c14e8f4738153d01b1f7333c5ec74117d5dc213
coder-sadhin <codersadhin@gmail.com>
2024-09-01 21:36:32 +0600

    checking

--- adjacency_matrix.cpp
+++ adjacency_matrix.cpp
@@ -13,6 +13,9 @@ int main()
         mat[a][b] = 1;
         mat[b][a] = 1;
     }
-    
+    if (mat[3][1] == 1)
+        cout << "connection ache";
+    else
+        cout << "connection nai";
     return 0;
 }
